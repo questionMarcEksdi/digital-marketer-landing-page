b7fa6eaa6279d0387f000b17c87e4c0945e2fa88
Mj Cole <itsmemarccole@gmail.com>
2022-03-05 14:06:42 +0800

    added hero image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   mode: 'jit',
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        image: `url('../images/pexels-fauxels-3184611.jpg')`,
+      },
+      backgroundSize: {
+        full: `100%`
+      },
+      backgroundRepeat: {
+        norepeat: `no-repeat`
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms')({
